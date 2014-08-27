33eeb44949b2f234d6884cf794fa231aa53e45f9
elizoller <zoller.eli@gmail.com>
2014-08-27 07:46:58 -0500

    adding test for no js

--- index.php
+++ index.php
@@ -38,42 +38,44 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
 </head>
 <body>
 <div class="container-fluid">
-<div class="row">
-  <h2><a href="index.php">The Best of Local: Discover What's Around You</a></h2>
-</div>
-<div class="row searchbox">
-  <div class="col-md-6 col-md-offset-3">
-  
-  <form name="search" method="post" action="results.php" role="form">
-    <div class="form-group form-group-lg">  
-      <div class="col-xs-8">
-        <input type="text" name="q" placeholder="Where are you?" id="city" class="form-control input-lg">
-      </div>
-      <input type="hidden" name="lat" id="lat">
-      <input type="hidden" name="lon" id="lon">
-      <div class="col-xs-4">
-       <button onclick="getLocation()" class="btn btn-info" type="button"><span class="glyphicon glyphicon-map-marker"></span></button>
-        <input type="submit" name="submit" value="Go" class="btn btn-success btn-lg">
-        </div>
-    </div>
-  </form>
-  <div class="alert alert-warning" role="alert" id="location_status"></div>
+  <div class="row">
+    <h2><a href="index.php">The Best of Local: Discover What's Around You</a></h2>
   </div>
+  <div class="row searchbox">
+    <div class="col-md-6 col-md-offset-3">
+      <noscript>
+        <br/>
+        <div class="alert alert-danger" role="alert">Your browser does not have Javascript enabled. Unfortunately The Best of Local requires javascript.</div>
+      </noscript>
+      <form name="search" method="post" action="results.php" role="form">
+        <div class="form-group form-group-lg">  
+          <div class="col-xs-8">
+            <input type="text" name="q" placeholder="Where are you?" id="city" class="form-control input-lg">
+          </div>
+          <input type="hidden" name="lat" id="lat">
+          <input type="hidden" name="lon" id="lon">
+          <div class="col-xs-4">
+            <button onclick="getLocation()" class="btn btn-info" type="button"><span class="glyphicon glyphicon-map-marker"></span></button>
+            <input type="submit" name="submit" value="Go" class="btn btn-success btn-lg">
+          </div>
+        </div>
+      </form>
+      <div class="alert alert-warning" role="alert" id="location_status"></div>
+    </div>
   </div>
   <p class="small text-right"><a href="https://farm4.staticflickr.com/3803/10012162166_cde34d427e_z_d.jpg">Image Copyright Nicolas Raymond</a></p>
-<div class="row about">
-  <div class="col-md-12">
-    <h3>What is The Best of Local?</h3>
-    <p>The Best of Local brings together cultural and entertainment information about a place into one interface. It combines information from <a href="http://en.wikipedia.org" target="_blank">Wikipedia</a>, restaurants and reviews from <a href="http://yelp.com" target="_blank">Yelp</a>, attractions with <a href="http://google.com" target="_blank">Google</a> Places, upcoming events with <a href="http://eventful.com" target="_blank">Eventful</a>, historical artifacts with the <a href="http://dp.la" target="_blank">Digital Public Library of America</a>, and recent pictures from <a href="http://flickr.com" target="_blank">Flickr</a>.</p>
-    <p>It's a veritable one stop shop for getting the scoop on your city or town!</p>
+  <div class="row about">
+    <div class="col-md-12">
+      <h3>What is The Best of Local?</h3>
+      <p>The Best of Local brings together cultural and entertainment information about a place into one interface. It combines information from <a href="http://en.wikipedia.org" target="_blank">Wikipedia</a>, restaurants and reviews from <a href="http://yelp.com" target="_blank">Yelp</a>, attractions with <a href="http://google.com" target="_blank">Google</a> Places, upcoming events with <a href="http://eventful.com" target="_blank">Eventful</a>, historical artifacts with the <a href="http://dp.la" target="_blank">Digital Public Library of America</a>, and recent pictures from <a href="http://flickr.com" target="_blank">Flickr</a>.</p>
+      <p>It's a veritable one stop shop for getting the scoop on your city or town!</p>
+    </div>
   </div>
-</div>
-<div class="made">
   <div class="row">
     <div class="col-md-12">
-    <h3>How is The Best of Local Made?</h3>
-    <p>The Best of Local uses HTML5, CSS3, Javascript, JQuery, PHP, Bootstrap, Less, and several APIs. I chose to use Bootstrap framework to provide a responsive grid. I used Less, a CSS preprocessor, to add custom styling and compress the CSS files for Bootstrap.</p>
-    <h4>APIs</h4>
+      <h3>How is The Best of Local Made?</h3>
+      <p>The Best of Local uses HTML5, CSS3, Javascript, JQuery, PHP, Bootstrap, Less, and several APIs. I chose to use Bootstrap framework to provide a responsive grid. I used Less, a CSS preprocessor, to add custom styling and compress the CSS files for Bootstrap.</p>
+      <h4>APIs</h4>
     </div>
   </div>
   <div class="row">
@@ -87,7 +89,7 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
         </div>
       </div>
     </div>
-     <div class="col-sm-4 col-md-3">
+    <div class="col-sm-4 col-md-3">
       <div class="thumbnail">
         <img src="https://s.yimg.com/pw/images/goodies/white-flickr.png" alt="flickr logo">
         <div class="caption">
@@ -97,7 +99,7 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
         </div>
       </div>
     </div>
-     <div class="col-sm-4 col-md-3">
+    <div class="col-sm-4 col-md-3">
       <div class="thumbnail">
         <img src="http://s3-media3.fl.yelpcdn.com/assets/2/www/img/2d7ab232224f/developers/yelp_logo_100x50.png" alt="Yelp Logo">
         <div class="caption">
@@ -107,7 +109,7 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
         </div>
       </div>
     </div>
-     <div class="col-sm-4 col-md-3">
+    <div class="col-sm-4 col-md-3">
       <div class="thumbnail">
         <img src="http://upload.wikimedia.org/wikipedia/commons/a/aa/Logo_Google_2013_Official.svg" alt="google logo">
         <a href="http://commons.wikimedia.org/wiki/File:Logo_Google_2013_Official.svg#mediaviewer/File:Logo_Google_2013_Official.svg">Licensed under Public domain via Wikimedia Commons</a>
@@ -166,25 +168,25 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
       </div>
     </div>
   </div>
-</div>
-<hr/>
-<div class="row">
-  <div class="col-sm-6">
-    <h3>About Me</h3>
-    <p>My name is Eli Zoller. I'm a graduate of <a href="http://simmons.edu" target="_blank">Simmons College</a> with a Masters in Library and Information Science. I currently work as Systems Librarian Fellow for Web and Digital Initiatives at the <a href="http://uta.edu" target="_blank">University of Texas at Arlington</a>. My main responsibility at the <a href="http://uta.edu/library" target="_blank">UT Arlington Libraries</a> is maintain and improve the existing libraries' websites and build additional sites as needed.</p>
-    <a href="http://www.eliwire.com" target="_blank" class="btn btn-primary" role="button">Personal Website</a>
-    <a href="http://github.com/elizoller" target="_blank" class="btn btn-success" role="button">Github</a>
-    <a href="http://www.eliwire.com/Resume_Zoller_Library.pdf" target="_blank" class="btn btn-info" role="button">Resume</a>
+
+  <hr/>
+  <div class="row">
+    <div class="col-sm-6">
+      <h3>About Me</h3>
+      <p>My name is Eli Zoller. I'm a graduate of <a href="http://simmons.edu" target="_blank">Simmons College</a> with a Masters in Library and Information Science. I currently work as Systems Librarian Fellow for Web and Digital Initiatives at the <a href="http://uta.edu" target="_blank">University of Texas at Arlington</a>. My main responsibility at the <a href="http://uta.edu/library" target="_blank">UT Arlington Libraries</a> is maintain and improve the existing libraries' websites and build additional sites as needed.</p>
+      <a href="http://www.eliwire.com" target="_blank" class="btn btn-primary" role="button">Personal Website</a>
+      <a href="http://github.com/elizoller" target="_blank" class="btn btn-success" role="button">Github</a>
+      <a href="http://www.eliwire.com/Resume_Zoller_Library.pdf" target="_blank" class="btn btn-info" role="button">Resume</a>
+    </div>
+    <div class="col-sm-6">
+      <h3><a href="https://github.com/elizoller/neu" target="_blank" >View the Source</a></h3>
+      <a href="https://github.com/elizoller/neu" target="_blank" ><img src="images/code.png" class="source"></a>
+    </div>
   </div>
-  <div class="col-sm-6">
-    <h3><a href="https://github.com/elizoller/neu" target="_blank" >View the Source</a></h3>
-    <a href="https://github.com/elizoller/neu" target="_blank" ><img src="images/code.png" class="source"></a>
+  <hr/>
+  <div class='footer'>
+    <p class='center-block'>Made by <a href='http://eliwire.com'>Eli Zoller</a></p>
   </div>
-</div>
-<hr/>
-<div class='footer'>
-  <p class='center-block'>Made by <a href='http://eliwire.com'>Eli Zoller</a></p>
-</div>
 </div>
 <script>
 $(function () 
