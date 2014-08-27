80068bb89b0de502bec69c3d8cb307ef9be58333
elizoller <zoller.eli@gmail.com>
2014-08-26 21:30:25 -0500

    adding info about api usage

--- README.md
+++ README.md
@@ -4,3 +4,22 @@ neu
 This site was built in Eli Zoller to demonstrate the use of open APIs in a single interface.
 
 To access the site go to <a href="http://eliwire.com/neu" target="_blank">eliwire.com/neu</a>
+
+APIs:
+<ul>
+<li><a href="http://dp.la/info/developers/codex/">Digital Public Library of America</a></li>
+<li><a href="http://api.eventful.com/">Eventful</a></li>
+<li><a href="https://www.flickr.com/services/api/">Flickr</a></li>
+<li><a href="http://www.geobytes.com/">GeoBytes</a></li>
+<li><a href="http://www.geonames.org/export/web-services.html">GeoNames</a></li>
+<li><a href="https://developers.google.com/places/documentation/">Google Places</a></li>
+<li><a href="http://www.mediawiki.org/wiki/API:Main_page">Wikipedia</a></li>
+<li><a href="http://www.yelp.com/developers/documentation">Yelp</a></li>
+</ul>
+
+Libraries and Frameworks:
+<ul>
+<li><a href="http://getbootstrap.com/">Bootstrap</a></li>
+<li><a href="http://jquery.com/">JQuery</a></li>
+<li><a href="http://lesscss.org/">Less</a></li>
+</ul>
--- index.php
+++ index.php
@@ -68,7 +68,7 @@
         <img src="http://epicenter.geobytes.com/images/Geobytes-red_small.gif" alt="GeoBytes logo">
         <div class="caption">
           <h3>GeoBytes</h3>
-          <p>The GeoBytes API was used for the autocomplete search box. JQuery was used get JSON from the Cities API.</p>
+          <p>The GeoBytes API was used for the autocomplete search box. JQuery was used get JSON from the Cities API. This process involved reverse geocoding.</p>
           <p><a href="http://www.geobytes.com/" class="btn btn-primary" role="button" target="_blank">API Info</a> </p>
         </div>
       </div>
@@ -132,7 +132,7 @@
         <img src="images/geonames.png" alt="geonames logo">
         <div class="caption">
           <h3>GeoNames</h3>
-          <p>GeoNames was used to gather required data about the specified location such as latitude and longitude coordinates.</p>
+          <p>GeoNames was used to gather required data about the specified location such as latitude and longitude coordinates, ie. geocoding.</p>
           <p><a href="http://www.geonames.org/export/web-services.html" class="btn btn-primary" role="button" target="_blank">API Info</a></p>
         </div>
       </div>
